60582103a45cfecd106c15b83680debdd8b62ae0
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-03-26 20:39:57 +0530

    sm8150-common: build qti powerhal and ship the appropriate powerhint
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- common.mk
+++ common.mk
@@ -422,6 +422,13 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := msmnile
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service-qti
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
