c876260ce2b36eaa2455f04e98d5905deae2632a
John Galt <johngaltfirstrun@gmail.com>
2021-10-12 09:57:12 -0400

    Revert "sm8150-common: kill power hal"
    This reverts commit 718b788e6204ac62090fc6654407489ce9c7ba21.

--- common.mk
+++ common.mk
@@ -360,6 +360,12 @@ PRODUCT_PACKAGES += \
     libstagefrighthw \
     libstagefright_softomx.vendor
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service-qti \
+    android.hardware.power@1.2.vendor \
+    vendor.qti.hardware.perf@2.2.vendor:64
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
