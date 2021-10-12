718b788e6204ac62090fc6654407489ce9c7ba21
Pranav <npv12@iitbbs.ac.in>
2021-10-12 11:58:24 +0530

    sm8150-common: kill power hal
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- common.mk
+++ common.mk
@@ -360,12 +360,6 @@ PRODUCT_PACKAGES += \
     libstagefrighthw \
     libstagefright_softomx.vendor
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power@1.2.vendor \
-    vendor.qti.hardware.perf@2.2.vendor:64
-
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
